4aff4ed1e0dfc32c45c586a3eb49d9c28eab5c5d
David Heilesen Danielewicz <daviddanielewicz@hotmail.com>
2015-11-17 15:33:05 +0100

    Program.cs lavet

---
+++ TDD/TDDApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Your name is: " + name + ". Correct?");
+            var answer = Console.ReadLine();
+            if (answer == "yes" || answer == "Yes")
+            {
+                Console.WriteLine("Good for you, that is a great name " + name);
+            }
+            else
+            {
+                Console.WriteLine("Could not read your name, sorry");
+            }
+        }
+    }
+}
--- TDD/TDDUnitTest/Class1.cs
+++ TDD/TDDUnitTest/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TDD
+namespace TDDUnitTest
 {
     public class Class1
     {
